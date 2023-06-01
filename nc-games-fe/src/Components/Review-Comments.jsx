1772e6c5a7bb0fd67fa982c2c41d92c42659dc4c
Maximilian Dent-Pooley <Max@Maximilians-MacBook-Pro-2.local>
2023-06-01 09:29:51 +0100

    refactored review-comments to remove dedicated state for comment length check and also a promise.all to resolve multiple set state functions in a single .then block

--- nc-games-fe/src/Components/Review-Comments.jsx
+++ nc-games-fe/src/Components/Review-Comments.jsx
@@ -4,36 +4,37 @@ import { fetchReviewComments } from "./utils";
 const ReviewComments = ({ reviewId }) => {
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [hasComments, setHasComments] = useState(false);
 
   useEffect(() => {
     fetchReviewComments(reviewId)
       .then((data) => {
-        setComments(data.comments);
-        setIsLoading(false);
-        setHasComments(data.comments.length > 0);
-      });
+        const { comments } = data;
+
+        return Promise.all([
+          setComments(comments),
+          setIsLoading(false),
+        ])
+      })
   }, [reviewId]);
 
-  if (isLoading) return <p>Loading comments...</p>;
-
-  if (!hasComments) {
-    return <p>No comments here yet!</p>
-  }
+  if (isLoading) return <p>Loading comments...</p>;  
 
   return (
     <div>
       <h4 tabIndex="0">Comments:</h4>
-      {comments.map((comment) => (
-  <div key={comment.comment_id} className="comment-box">
-  <p className="comment-user">User: {comment.author}</p>
-  <p className="comment-body">Comment: {comment.body}</p>
-  <p className="comment-votes">Upvotes: {comment.votes}</p>
-  <p className="comment-date">Timestamp: {comment.created_at}</p>
-</div>
-    ))}
+      {comments.length > 0 ? (
+        comments.map((comment) => (
+          <div key={comment.comment_id} className="comment-box">
+            <p className="comment-user">User: {comment.author}</p>
+            <p className="comment-body">Comment: {comment.body}</p>
+            <p className="comment-votes">Upvotes: {comment.votes}</p>
+            <p className="comment-date">Timestamp: {comment.created_at}</p>
+          </div>
+        ))
+      ) : (
+        <p>No comments here yet!</p>
+      )}
     </div>
   );
-};
-
+      }  
 export default ReviewComments;
